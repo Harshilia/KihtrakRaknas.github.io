d3800c48ea417a402478d261868261467c139770
Harshil <71715890+Harshilia@users.noreply.github.com>
2020-11-23 08:19:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logofr" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
